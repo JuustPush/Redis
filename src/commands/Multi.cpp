e265779cfdcb98a9df214e537035c8b0ece048e8
Your Name <juustpush1@gmail.com>
2025-01-03 18:37:38 -0500

    codecrafters submit [skip ci]

--- src/commands/Multi.cpp
+++ src/commands/Multi.cpp
@@ -5,10 +5,7 @@
 namespace commands {
 
 std::optional<std::string> Multi::inner_handle(const std::span<const std::string>& params, Session* session) {
-  int num_params = params.size();
-  if (num_params < 1) {
-    return std::nullopt;
-  }
+  
 
   
   
